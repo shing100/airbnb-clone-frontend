5f2057dcca343ee6c3b649ee5e6ccdd7d1415bf9
Lim Geun <shing100@naver.com>
2023-08-14 18:54:51 +0900

    Sign Up modal

---
+++ src/components/SocialLogin.tsx
@@ -0,0 +1,18 @@
+import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
+import { FaGithub, FaComment } from "react-icons/fa";
+
+export default function SocialLogin() {
+    return (
+        <Box mb={4}>
+            <HStack my={8}>
+                <Divider />
+                <Text textTransform={"uppercase"} color={"gray.500"} fontSize={"xs"} as={"b"}>Or</Text>
+                <Divider />
+            </HStack>
+            <VStack>
+                <Button leftIcon={<FaGithub />} colorScheme={"blackAlpha"} w="100%">Continue with Github</Button>
+                <Button leftIcon={<FaComment />} colorScheme={"yellow"} w="100%">Continue with Kakao</Button>
+            </VStack>
+        </Box>
+    )
+}
